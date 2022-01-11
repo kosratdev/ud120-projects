1e82a36c41e21efec6625f118c60936044cbba24
Kosrat D. Ahmed <kosrat.d.ahmad@gmail.com>
2022-01-11 22:29:04 +0300

    Complete nb_author_id mini project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,9 +9,10 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+from sklearn.naive_bayes import GaussianNB
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -21,11 +22,18 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
 ##############################################################
 # Enter Your Code Here
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
 
+t0 = time()
+clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
 
+print(clf.score(features_test, labels_test))
 
 ##############################################################
 
